310bfd87b89cd312f12044d7acba6a2ecaf31a63
marsiocons <maaarciosk8@gmail.com>
2021-05-14 10:25:47 -0300

    Ejercicio1. hasta acá sin errores

--- plantilla.py
+++ plantilla.py
@@ -1,15 +1,37 @@
 ################
-# Martín René - @martinvilu
+# Marcio Betanzo - @marsiocons
 # Plantilla de ejercicio
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
 
+class IngresoIncorrecto(Exception):
+    """El dato ingresado no es correcto."""
+    pass 
 
-# Reemplazar por las funciones del ejercicio
+def intentar_ingreso_numero(numIng):
+    
+    i = 0
+    while (i < 5):
+        NumIn = input("Ingrese un valor: ")
+        try:
+            int(NumIn)
+            num = int(NumIn)
+            print("Es un número!")
+            if (type(num) == int):
+                #ingreso_entero_restringido()
+                break
+        except ValueError:
+            print("Error: el dato ingresado no es un número!")
+            print(f"Reintentar ({i+1} /5)")
+            i = i + 1
+    
+#def ingreso_entero_reintento(mensaje, cantidad_reintentos=5):
+#    print(mensaje)
 
-
+def ingreso_entero_restringido(mensaje,valor_minimo=0, valor_maximo=10):
+    print(mensaje)
 def prueba():
-    pass
-
+    intentar_ingreso_numero(12)
+    
 if __name__ == "__main__":
     prueba()
