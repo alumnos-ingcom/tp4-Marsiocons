39aac0e12608e6c41f7d2041ea7d48d2df2b1398
marsiocons <maaarciosk8@gmail.com>
2021-05-18 17:35:29 -0300

    tp4-eje1.py re-escrito.

---
+++ [old]tp4-ej1.py
@@ -0,0 +1,53 @@
+################
+# Marcio Betanzo - @marsiocons
+# Plantilla de ejercicio
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+class IngresoIncorrecto(Exception):
+    """Esta es la Excepcion para el ingreso incorrecto"""
+    pass 
+
+def conversion_int(dato, intentos):
+    while intentos > 0:
+        if intentos <= 0:
+            break
+        try:
+            dato_int = int(dato)
+            if ( ingreso_entero_restringido(dato_int) ):
+                print(f"El número cumple con las características. ( {dato_int} )")
+                break
+            else:
+                ingreso_entero_reintento(dato, intentos)
+                break
+        except ValueError as err:
+            raise IngresoIncorrecto("No se puede transformar")
+            ingreso_entero_reintento(dato, intentos)
+            #print("No se puede transformar")
+            
+            return
+
+def ingreso_entero_reintento(dato, reintentos):
+    print(f"El dato es inválido: ( {dato} )")
+    print(f"Intentos restantes: {reintentos - 1}")
+    reintentos = reintentos - 1
+    if (reintentos > 0):
+        pedir_dato(reintentos)
+
+def ingreso_entero_restringido(dato, dato_min=0, dato_max=10):
+    if (dato >= dato_min and dato <= dato_max):
+        return True
+    else:
+        return False
+
+def pedir_dato(intentos=5):
+    dato_new= input("Ingrese un dato: ")
+    conversion_int(dato_new, intentos)
+    
+
+def prueba():
+    pedir_dato()
+    pass
+
+if __name__ == "__main__":
+    prueba()
--- tp4-ej1.py
+++ tp4-ej1.py
@@ -6,45 +6,19 @@
 
 class IngresoIncorrecto(Exception):
     """Esta es la Excepcion para el ingreso incorrecto"""
-    pass 
+    pass
 
-def conversion_int(dato, intentos):
-    while intentos > 0:
-        if intentos <= 0:
-            break
-        try:
-            dato_int = int(dato)
-            if ( ingreso_entero_restringido(dato_int) ):
-                print(f"El número cumple con las características. ( {dato_int} )")
-                break
-            else:
-                ingreso_entero_reintento(dato, intentos)
-                break
-        except ValueError:
-            print("No se puede transformar")
-            ingreso_entero_reintento(dato, intentos)
-            return
+def ingreso_entero(entrada):
+    ingreso = input(f"{entrada} #")
+    try:
+        salida = int(ingreso)
+    except ValueError as err:
+        raise IngresoIncorrecto("No se puede transformar") from err
+    return salida
 
-def ingreso_entero_reintento(dato, reintentos):
-    print(f"El dato es inválido: ( {dato} )")
-    print(f"Intentos restantes: {reintentos - 1}")
-    reintentos = reintentos - 1
-    if (reintentos > 0):
-        pedir_dato(reintentos)
-
-def ingreso_entero_restringido(dato, dato_min=0, dato_max=10):
-    if (dato >= dato_min and dato <= dato_max):
-        return True
-    else:
-        return False
-
-def pedir_dato(intentos=5):
-    dato_new= input("Ingrese un dato: ")
-    conversion_int(dato_new, intentos)
-    
 
 def prueba():
-    pedir_dato()
+    print(ingreso_entero("Ingrese un número"))
     pass
 
 if __name__ == "__main__":
