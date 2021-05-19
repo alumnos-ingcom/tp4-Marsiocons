6ba988ac64db1f109f76b95e3dc247cba2703265
marsiocons <maaarciosk8@gmail.com>
2021-05-19 12:15:28 -0300

    tp4ej2.py terminado! (cuatro if para comprobar si es positivo o negativo)

--- tp4ej2.py
+++ tp4ej2.py
@@ -4,15 +4,39 @@
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
 
+import tp4ej1 as ing
+
 def suma_lenta(numero, otro_numero):
     resultado = numero + otro_numero
-    print(resultado)
-    while numero != resultado:
-        print(numero)
-        numero = numero + 1
+    
+    #### si ambos números son positivos ####
+    if (numero > 0 < otro_numero):
+        while numero <= resultado:
+            print(numero)
+            numero = numero + 1
+            
+    #### si el primer número es positivo y el segundo negativo ####
+    if (numero > 0 > otro_numero):
+        while numero >= resultado:
+            print(numero)
+            numero = numero - 1
+            
+    #### si el primer número es negativo y el segundo positivo ####
+    if (numero < 0 < otro_numero):
+        while numero <= resultado:
+            print(numero)
+            numero = numero + 1
+            
+    #### si ambos números son negativos ####
+    if (numero < 0 > otro_numero):
+        while numero >= resultado:
+            print(numero)
+            numero = numero - 1
 
 def prueba():
-    suma_lenta(12,2)
+    num1 = ing.ingreso_entero("Ingrese un número:")
+    num2 = ing.ingreso_entero("Ingrese un número:")
+    suma_lenta(num1, num2)
     pass
 
 if __name__ == "__main__":
