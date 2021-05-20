77f071a1bb4c2b5acc826257f3f2852e1c3831ad
marsiocons <maaarciosk8@gmail.com>
2021-05-20 09:42:26 -0300

    Eliminado print() de cada if else. Agregado un return al final.

--- tp4ej2.py
+++ tp4ej2.py
@@ -12,31 +12,29 @@ def suma_lenta(numero, otro_numero):
     #### si ambos números son positivos ####
     if (numero >= 0 <= otro_numero):
         while numero <= resultado:
-            print(numero)
             numero = numero + 1
             
     #### si el primer número es positivo y el segundo negativo ####
     if (numero >= 0 > otro_numero):
         while numero >= resultado:
-            print(numero)
             numero = numero - 1
             
     #### si el primer número es negativo y el segundo positivo ####
     if (numero < 0 <= otro_numero):
         while numero <= resultado:
-            print(numero)
             numero = numero + 1
             
     #### si ambos números son negativos ####
     if (numero < 0 > otro_numero):
         while numero >= resultado:
-            print(numero)
             numero = numero - 1
+            
+    return resultado
 
 def prueba():
     num1 = ing.ingreso_entero("Ingrese un número:")
     num2 = ing.ingreso_entero("Ingrese un número:")
-    suma_lenta(num1, num2)
+    print(suma_lenta(num1, num2))
     pass
 
 if __name__ == "__main__":
