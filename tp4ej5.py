28ad881c796d9319668288bd99120af120e12e00
marsiocons <maaarciosk8@gmail.com>
2021-05-20 20:09:46 -0300

    Ejercicio 5 terminado. Imprime si es neg, pos o 0. retorna -1, 1 y 0 respectivamente.

---
+++ tp4ej5.py
@@ -0,0 +1,27 @@
+################
+# Marcio Betanzo - @marsiocons
+# Plantilla de ejercicio
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+import tp4ej1 as ing
+
+def signo(numero):
+    if (numero == 0):
+        print("El número es 0!")
+        return 0
+    if (numero > 0):
+        print("El número es positivo!")
+        return 1
+    if (numero < 0):
+        print("El número es negativo!")
+        return -1
+
+def prueba():
+    num = ing.ingreso_entero("""Ingrese un número para determinar si es
+    negativo, positivo o cero.""")
+    print(signo(num))
+    
+    pass
+
+if __name__ == "__main__":
+    prueba() 
