7f50fde2f35983126a1a497549bab845f71b527f
marsiocons <maaarciosk8@gmail.com>
2021-05-21 17:35:03 -0300

    Ejercicio 7 terminado. No realiza la operación con decimales

--- tp4ej7.py
+++ tp4ej7.py
@@ -4,42 +4,72 @@
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
 
+import tp4ej1 as ing
+
 def division_lenta(dividendo, divisor):
+    if (divisor == 0):
+        raise ing.IngresoIncorrecto("Error! El divisor no puede ser 0!")
+    
+    if (dividendo < 0):
+        dividendo = dividendo * -1
+        
+    if (divisor < 0):
+        divisor = divisor * -1
+        
     cociente = 0
-    decimal = False
+    
     while (dividendo > 0):
-        #print(dividendo)
-        resto = dividendo
         dividendo = dividendo - divisor
         if (dividendo > 0):
             cociente = cociente + 1
-        elif (dividendo == 0):
-            cociente = cociente + 1
-            return print(f"el cociente es {cociente} y tiene resto 0")
-        else:
-            print("entré en el else")
-            decimal = True
-    if (decimal == True):
-        print("entré al decimal")
-        dividendo = resto * 10
-        print(f"el dividendo es {dividendo}")
-        while (dividendo > 0):
             resto = dividendo
-            dividendo = dividendo - divisor
-            print(f"el dividendo es ahora {dividendo}")
-            if dividendo == 0:
-                resto = 0
-            cociente = cociente + 0.1
+        elif(dividendo == 0):
+            resto = 0
+            cociente = cociente +1
+        else:
+            resto = dividendo + divisor
+        resto_cociente = [resto, cociente]
+    return resto_cociente
             
-                
-            
-    print(f"El cociente de la operación es: {cociente}")
-    print(f"El resto de la operación es: {resto}")
+
+# def division_lenta(dividendo, divisor):
+#     cociente = 0
+#     decimal = False
+#     while (dividendo > 0):
+#         #print(dividendo)
+#         resto = dividendo
+#         dividendo = dividendo - divisor
+#         if (dividendo > 0):
+#             cociente = cociente + 1
+#         elif (dividendo == 0):
+#             cociente = cociente + 1
+#             return print(f"el cociente es {cociente} y tiene resto 0")
+#         else:
+#             print("entré en el else")
+#             decimal = True
+#     if (decimal == True):
+#         print("entré al decimal")
+#         dividendo = resto * 10
+#         print(f"el dividendo es {dividendo}")
+#         while (dividendo > 0):
+#             resto = dividendo
+#             dividendo = dividendo - divisor
+#             print(f"el dividendo es ahora {dividendo}")
+#             if dividendo == 0:
+#                 resto = 0
+#             cociente = cociente + 0.1
+#             
+#                 
+#             
+#     print(f"El cociente de la operación es: {cociente}")
+#     print(f"El resto de la operación es: {resto}")
         
         
 
 def prueba():
-    division_lenta(17,5)
+    num1 = ing.ingreso_entero("Ingrese el dividendo de la operación: ")
+    num2 = ing.ingreso_entero("Ingrese el divisor de la operación: ")
+    print(division_lenta(num1,num2))
     pass
 
 if __name__ == "__main__":
